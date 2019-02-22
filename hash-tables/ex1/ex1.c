c3f7d04a5933e205d70deb1ad93e05a7f5f85433
Linh Bouniol <paulalinh89@gmail.com>
2019-02-22 08:10:55 -0800

    Implemented ht-ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -7,7 +7,29 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
 
-  // YOUR CODE HERE
+  for (int index = 0; index < length; index++) {
+    int weight = weights[index];
+    int oppositeWeight = limit - weight;
+    
+    // Get the index, if there is nothing in the hash table, returns -1
+    int oppositeWeightIndex = hash_table_retrieve(ht, oppositeWeight);
+    
+    // If there is an index, set the indices
+    if (oppositeWeightIndex != -1) {
+      Answer *answer = malloc(sizeof(Answer));
+      
+      // At this point the current index is not in the hash table, and it is the larger index
+      answer->index_1 = index;
+      answer->index_2 = oppositeWeightIndex;
+      
+      return answer;
+    }
+    
+    // Add the weight at the current index to the hash table
+    hash_table_insert(ht, weight, index);
+  }
+  
+  destroy_hash_table(ht);
 
   return NULL;
 }
